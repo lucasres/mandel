475098deff7a9162f0b1f42bb58e2810f6733faa
mandala21 <lucasresone@gmail.com>
2020-04-11 00:32:37 -0300

    Layout de config ok

--- src/pages/Config/index.js
+++ src/pages/Config/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import { FiArrowLeft } from "react-icons/fi";
 
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 export default function Config() {
   return (
     <div className="config-wrapper">
         <div className="row h100">
             <div className="menu-settings">
-                <p className="title">Configurações</p>
+                <div className="title-content">
+                    <Link to="/">
+                        <FiArrowLeft className="icon"/>
+                    </Link>
+                    <p className="title">Configurações</p>
+                </div>
                 <ul>
-                    <li>
-                        Comando
+                    <li className="active">
+                        Geral
                     </li>
                 </ul>
             </div>
             <div className="content-settings">
-                <p>Settings</p>
+                <p>Caminho do projeto:</p>
+                <input className="input-txt" placeholder="Clique aqui para selecionar o caminho..." webkitdirectory />
             </div>
         </div>
     </div>
